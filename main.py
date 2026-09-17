f052d8735d0d1f5cdcc03db37e2cc0c2853b8a8e
Samuel Felipe <samuel.f.cardoso6@aluno.senai.br>
2026-09-17 10:25:37 -0300

    feat: adiciona listagem de alunos

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ def cadastrar():
     if nome == "":
         print("O nome não pode ficar vazio.")
         return
-
-
     for aluno in alunos:
         if aluno.lower() == nome.lower():
             print("Aluno já cadastrado.")
             return
     alunos.append(nome)
     print("Aluno cadastrado.")
+def listar():
+    for aluno in alunos:
+        print(aluno)
 while True:
     print("\n--- CADASTRO DE ALUNOS ---")
     print("1 - Cadastrar")
+    print("2 - Listar")
     print("0 - Sair")
     opcao = input("Escolha: ")
     if opcao == "0":
@@ -22,5 +24,7 @@ while True:
         break
     elif opcao == "1":
         cadastrar()
+    elif opcao == "2":
+        listar()
     else:
         print("Opção inválida.")
